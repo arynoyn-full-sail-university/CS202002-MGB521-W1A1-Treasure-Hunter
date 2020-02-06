a57487f471da58caed3d88e9bf6af6e9bd3dec82
Christopher West <chris.west@codegeekllc.com>
2020-02-06 15:18:05 -0700

    Added xml layout, implemented View.Onclick, imported resources provided by class
    Added xml layout, implemented View.Onclick, imported resources provided by class

--- app/src/main/java/edu/fullsail/mgems/cse/treasurehunter/christopherwest/MainActivity.java
+++ app/src/main/java/edu/fullsail/mgems/cse/treasurehunter/christopherwest/MainActivity.java
@@ -3,12 +3,20 @@ package edu.fullsail.mgems.cse.treasurehunter.christopherwest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.btnCredits).setOnClickListener(this);
+        findViewById(R.id.btnInventory).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
